b8185e3783aeb9e70b24d8c3627457186156ee21
davinci2015 <dvincija@foi.hr>
2015-11-18 15:24:44 +0100

    Sminkanje login i reg forme

--- www/app/scripts/app.js
+++ www/app/scripts/app.js
@@ -16,10 +16,9 @@ angular
   ])
   .config(['$httpProvider', '$routeProvider', function ($httpProvider, $routeProvider) {
     
-   //$httpProvider.interceptors.push('loginInterceptor');
+    //$httpProvider.interceptors.push('loginInterceptor');
     
-     $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
     $routeProvider
       .when('/', {
