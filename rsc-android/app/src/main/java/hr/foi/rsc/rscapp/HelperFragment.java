f0f391e6a1cdcb97dabe7a3744261a69912528d1
hhrvoic <hhrvoic@foi.hr>
2015-11-22 00:19:14 +0100

    intermezzo fragmenthelp

--- rsc-android/app/src/main/java/hr/foi/rsc/rscapp/HelperFragment.java
+++ rsc-android/app/src/main/java/hr/foi/rsc/rscapp/HelperFragment.java
@@ -1,9 +1,50 @@
 package hr.foi.rsc.rscapp;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 /**
  * Created by hrvoje on 21/11/15.
  */
+
 public class HelperFragment extends Fragment {
+
+    TextView seconds;
+    TextView minutes;
+
+    void ajmo(){
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                seconds.setText(String.format("%d", millisUntilFinished / 1000));
+                minutes.setText(String.format("%d", millisUntilFinished / 1000/60));
+            }
+
+            public void onFinish() {
+                //big boom!
+            }
+        }.start();
+
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       // setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        View v = inflater.inflate(R.layout.fragment_helper, container, false);
+        seconds=(TextView) v.findViewById(R.id.txtViewCtrSeconds);
+        minutes=(TextView) v.findViewById(R.id.textViewCtrMinutes);
+        ajmo();
+        return v;
+    }
+
+
 }
