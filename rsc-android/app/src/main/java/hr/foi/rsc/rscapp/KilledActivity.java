21ab835e91c78e3b97f3863b953ec97be7bb6e3f
hhrvoic <hhrvoic@foi.hr>
2015-11-22 03:12:47 +0100

    predlozak za KilledActivity treba doraditi!

---
+++ rsc-android/app/src/main/java/hr/foi/rsc/rscapp/KilledActivity.java
@@ -0,0 +1,13 @@
+package hr.foi.rsc.rscapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KilledActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_killed);
+    }
+}
---
+++ rsc-android/app/src/main/java/hr/foi/rsc/rscapp/fragments/KilledFragmentStat.java
@@ -0,0 +1,10 @@
+package hr.foi.rsc.rscapp.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by hrvoje on 22/11/15.
+ */
+public class KilledFragmentStat extends Fragment {
+
+}
